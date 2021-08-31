982bd85ab414c53a68e83f95fdc6a27d65059f02
Dan Ross <daniellross@gmail.com>
2021-08-31 15:48:49 -0500

    Phase2 program checks user input

--- Program.cs
+++ Program.cs
@@ -6,5 +6,15 @@ void Main()
 {
     Console.WriteLine("Hello! You're playing the guesssing game. Try to guess the secret number!");
     string userGuess = Console.ReadLine();
-    Console.WriteLine($@"You guessed {userGuess}.");
+    // TODO: add check for user guess
+    int secretNumber = 42;
+    bool correctGuess = secretNumber == Int32.Parse(userGuess);
+    if (correctGuess)
+    {
+        Console.WriteLine("You guessed Correctly");
+    }
+    else
+    {
+        Console.WriteLine("WRONG");
+    }
 }
